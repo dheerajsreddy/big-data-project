b3d4f65a512aefb0859b83d3cf51a7dc08d77256
Karthik v <56793779+karthik200116@users.noreply.github.com>
2021-12-06 23:57:43 +0530

    MiniBatchKMeans final
    KMeans clustering using MiniBatchKMeans

---
+++ Final Files/kmeans.py
@@ -0,0 +1,111 @@
+import warnings
+warnings.filterwarnings('ignore') 
+import numpy as np
+import sys, pyspark, json,pickle
+from sklearn import metrics
+from pyspark import SparkContext
+from pyspark.sql.functions import *
+from sklearn.pipeline import Pipeline
+from pyspark.streaming import StreamingContext
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from pyspark.sql import SQLContext, SparkSession,Row,Column
+from sklearn.cluster import MiniBatchKMeans
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#creating a spark context
+sc = SparkContext("local[2]", "Sentiment")
+
+#creating a streaming context to read the incoming streaming data
+ssc = StreamingContext(sc, 1)
+sql_context=SQLContext(sc)
+
+predictions_list = []
+
+#function to convert the datastream converted to list into a dataframe object for further processing
+def to_df(data):
+	if data.isEmpty():
+		return
+	
+	#creating of spark session for the streaming data input
+	ss = SparkSession(data.context)
+	data = data.collect()[0]
+	columns = [f"feature{i}" for i in range(len(data[0]))]
+	
+	#create dataframe from the streaming data
+	df = ss.createDataFrame(data, columns)
+	df.show()
+
+	#feature1 = df.select('feature1').rdd.map(lambda r: r(0).asInstanceOf[String]).collect()
+	#feature0 = df.select('feature0').rdd.map(lambda r: r(0).asInstanceOf[String]).collect()
+	
+	#pre-processing of data
+	pre_process(df)
+
+def pre_process(df):
+
+
+	feature1 = np.array(df.select('feature1').collect())
+	feature1 = feature1.flatten()
+	feature0 = np.array(df.select('feature0').collect())
+	feature0 = feature0.flatten()
+
+	'''
+	# 1. CountVectorize the data
+	vect = CountVectorizer()
+	X_train_counts = vect.fit_transform(feature1)
+
+	# 2. Hashing TF-IDF
+	tfidf_transformer = TfidfTransformer()
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+	
+	kmeans = MiniBatchKMeans(n_clusters=2,init = 'k-means++',random_state=0, batch_size=1000,verbose = 0,max_iter = 1000)
+	kmeans = kmeans.partial_fit(X_train_tfidf)
+	#kmeans = kmeans.partial_fit(train_data_x)
+	
+	pipeline = Pipeline(([('vect', CountVectorizer()),('tfidf', TfidfTransformer()), ('kmeans', MiniBatchKMeans())]))
+	pipeline.fit(feature1)
+
+	with open('passive_aggr.pkl','wb') as f:
+	    pickle.dump(pipeline,f)
+	print("****** DONE TRAINING *******")
+	
+	'''
+	
+	with open('passive_aggr.pkl', 'rb') as f:
+		clf2 = pickle.load(f)
+	predicted = clf2.predict(feature1)
+	#sns.stripplot(x=)
+	print(metrics.classification_report(feature0, predicted))
+	
+	
+#function to convert the datastream to a list
+def map_data(data):
+
+	#load the incoming json file
+	
+	json_data=json.loads(data)
+	list_rec = list()
+	
+	#convert the json file to tupple which is appended to a list and returned
+	for rec in json_data:
+		to_tuple = tuple(json_data[rec].values())
+		list_rec.append(to_tuple)
+	return list_rec 	
+
+
+def plot_result(x_name, label_name, df):
+	# sns.scatterplot(data=df, x=range(len(df)), y=x_name, hue=label_name)
+	sns.stripplot(x=label_name, y=x_name, data=df)
+	plt.show()
+
+
+#creating a socket to read the data
+lines = ssc.socketTextStream("localhost",6100).map(map_data).foreachRDD(to_df)
+
+#start streaming
+ssc.start() 
+ssc.awaitTermination()
